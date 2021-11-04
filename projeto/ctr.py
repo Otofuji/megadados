a2b1b5a6a5e35ffa2018f0feec7e2ff01ea3bda2
Eric Fernando Otofuji Abrantes <otofuji@MacBook-Pro-de-Eric.local>
2021-11-04 13:15:57 -0300

    Esqueleto inicial do projeto

---
+++ projeto/ctr.py
@@ -0,0 +1,128 @@
+# git@github.com:Otofuji/megadados.git
+# https://fastapi.tiangolo.com
+# async version
+
+#####################################
+# REQUIREMENTS
+# $ python -m pip install --upgrade pip
+# $ pip install fastapi
+# $ pip install "uvicorn[standard]"
+
+# RUN SERVER USING
+# $ uvicorn ctr:notas --reload
+
+#                             CHECKLIST
+#
+# [ ] POST        REQ-01      usuário pode criar disciplina
+# [ ] GET         REQ-02      disciplina tem nome único (obrigatório)
+# [ ] GET         REQ-03      disciplina tem nome de professor (opcional)
+# [ ] GET         REQ-04      disciplina tem campo de anotação livre (texto)
+# [ ] DELETE      REQ-05      usuário pode deletar disciplina
+# [ ] GET         REQ-06      usuário pode listar os nomes de suas disciplinas
+# [ ] PUT         REQ-07      usuário pode modificar as informações de uma disciplina, incluindo seu nome
+# [ ] POST        REQ-08      usuário pode adicionar uma nota a uma disciplina
+# [ ] DELETE      REQ-09      usuário pode deletar uma nota de uma disciplina
+# [ ] GET         REQ-10      usuário pode listar as notas de uma disciplina
+# [ ] PUT         REQ-11      usuário pode modificar uma nota de uma disciplina
+#
+#####################################
+
+from typing import Optional
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+notas = FastAPI()
+
+class Disciplina(BaseModel):
+    name: str = Field(..., example="Megadados")
+    description: Optional[str] = Field(None, example="DBA")
+    professor: Optional[str] = Field(None, example="Fábio Ayres")
+    annotation: Optional[str] = Field(None, example="Lorem ipsum dolor sit amet")
+    ## adicionar campo de notas que atenda aos requerimentos - mais de uma nota por disciplina
+
+    #TODO corrigir schema
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+""" 
+#DO TUTORIAL - INICIO
+@notas.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@notas.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item_id": item_id, "q": q}
+
+@notas.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id} 
+
+#DO TUTORIAL - FIM
+ """
+
+ 
+
+#TODO REQ-01
+@notas.post("/disciplina/{Disciplina}")
+async def CriaDisciplinas(name: str, description: Optional[str], professor: Optional[str], annotation: Optional[str]):
+    return None
+
+#TODO REQ-02
+@notas.get("/disciplina/{Disciplina}")
+async def DisciplinaNomes(args):
+    return None
+
+#TODO REQ-03
+@notas.get("/disciplina/{Disciplina}")
+async def DisciplinaProfessores(args):
+    return None
+
+#TODO REQ-04
+@notas.get("/disciplina/{Disciplina}")
+async def DisciplinaTextos(args):
+    return None
+
+#TODO REQ-05
+@notas.delete("/disciplina/{Disciplina}")
+async def ApagaDisciplinas(args):
+    return None
+
+#TODO REQ-06
+@notas.get("/disciplina/{Disciplina}")
+async def ListaDisciplinas(args):
+    return None
+
+#TODO REQ-07
+@notas.put("/disciplina/{Disciplina}")
+async def AtualizaDisciplinas(name: str, description: Optional[str], professor: Optional[str], annotation: Optional[str]):
+    return None
+
+#TODO REQ-08
+@notas.post("/disciplina/{Disciplina}")
+async def PublicaNotas(args):
+    return None
+
+#TODO REQ-09
+@notas.delete("/disciplina/{Disciplina}")
+async def ApagaNotas(args):
+    return None
+
+#TODO REQ-10
+@notas.get("/disciplina/{Disciplina}")
+async def ListaNotas(args):
+    return None
+
+#TODO REQ-11
+@notas.put("/disciplina/{Disciplina}")
+async def AtualizaNotas(args):
+    return None
--- tutorial/main.py
+++ tutorial/main.py
@@ -13,15 +13,38 @@
 #####################################
 
 from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
 
 app = FastAPI()
 
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
 class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
+    name: str = Field(..., example="Foo")
+    description: Optional[str] = Field(None, example="A very nice Item")
+    price: float = Field(..., example=35.4)
+    tax: Optional[float] = Field(None, example=3.2)
+
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
 
 @app.get("/")
 async def read_root():
@@ -30,8 +53,10 @@ async def read_root():
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
     return {"item_id": item_id, "q": q}
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_name": item.name, "item_id": item_id} 
