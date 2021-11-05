7bb0414f2b0b2cbbd040cc0bc69c9869fed77713
Eric Fernando Otofuji Abrantes <otofuji@MacBook-Pro-de-Eric.local>
2021-11-05 15:02:51 -0300

    REQ-04

--- projeto/ctr.py
+++ projeto/ctr.py
@@ -73,11 +73,6 @@ async def CriaDisciplinas(course: str, description: Optional[str], professor: Op
     return {"course": course} 
 
 
-#TODO REQ-04
-@notas.get("/disciplina/{course}")
-async def DisciplinaTextos(args):
-    return None
-
 #TODO REQ-05
 @notas.delete("/disciplina/{course}")
 async def ApagaDisciplinas(args):
